581030edbbced2fde414226bea62d5dc33435b47
TaehunKim0 <jack071000@naver.com>
2024-04-09 09:39:33 +0900

    fix: formatting

--- lua/plugins/nvim-drop.lua
+++ lua/plugins/nvim-drop.lua
@@ -5,12 +5,11 @@ return
   config = function()
     require("drop").setup {
       ---@type DropTheme|string
-  theme = "snow", -- can be one of rhe default themes, or a custom theme
-  max = 40, -- maximum number of drops on the screen
-  interval = 150, -- every 150ms we update the drops
-  screensaver = 1000 * 60 * 3, -- show after 5 minutes. Set to false, to disable
-  filetypes = { "dashboard", "alpha", "starter" }, -- will enable/disable automatically for the following filetypes
+      theme = "snow",                              -- can be one of rhe default themes, or a custom theme
+      max = 40,                                    -- maximum number of drops on the screen
+      interval = 150,                              -- every 150ms we update the drops
+      screensaver = 1000 * 60 * 3,                 -- show after 5 minutes. Set to false, to disable
+      filetypes = { "dashboard", "alpha", "starter" }, -- will enable/disable automatically for the following filetypes
     }
-
   end
 }
